2f5af229619b5ba0e04f117002eebd90910146c5
phamducai <99159542+phamducai@users.noreply.github.com>
2022-05-25 10:11:14 +0700

    haha

--- src/components/Chart/Aderelaychartcomponent/historyAde/HistoryAderelayChart.Irms.js
+++ src/components/Chart/Aderelaychartcomponent/historyAde/HistoryAderelayChart.Irms.js
@@ -1,58 +1,58 @@
-import React from "react";
-import ReactApexChart from "react-apexcharts";
+// import React from "react";
+// import ReactApexChart from "react-apexcharts";
 
-import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
-import { getRecords } from "../../../../api";
-import { Typography } from "@mui/material";
+// import { useParams } from "react-router-dom";
+// import { useQuery } from "react-query";
+// import { getRecords } from "../../../../api";
+// import { Typography } from "@mui/material";
 
-function HistoryAderelayChartIrms() {
-  const { deviceId } = useParams();
-  const { data: irms } = useQuery(
-    "Records1",
-    () => getRecords(deviceId, "irms1"),
-    {
-      initialData: [],
-    }
-  );
-  const series = [
-    {
-      data: irms.map(({ timestamp, value }) => ({
-        x: timestamp,
-        y: value,
-      })),
-    },
-  ];
-  const options = {
-    chart: {
-      height: 350,
-      type: "area",
-      dataLabels: {
-        enabled: false,
-      },
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "datetime",
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy HH:mm",
-      },
-    },
-  };
-  return (
-    <React.Fragment>
-      <Typography align="center">Irms</Typography>
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="area"
-        height={350}
-      />
-    </React.Fragment>
-  );
-}
-export default HistoryAderelayChartIrms;
+// function HistoryAderelayChartIrms() {
+//   const { deviceId } = useParams();
+//   const { data: irms } = useQuery(
+//     "Records1",
+//     () => getRecords(deviceId, "irms"),
+//     {
+//       initialData: [],
+//     }
+//   );
+//   const series = [
+//     {
+//       data: irms.map(({ timestamp, value }) => ({
+//         x: timestamp,
+//         y: value,
+//       })),
+//     },
+//   ];
+//   const options = {
+//     chart: {
+//       height: 350,
+//       type: "area",
+//       dataLabels: {
+//         enabled: false,
+//       },
+//     },
+//     stroke: {
+//       curve: "smooth",
+//     },
+//     xaxis: {
+//       type: "datetime",
+//     },
+//     tooltip: {
+//       x: {
+//         format: "dd/MM/yy HH:mm",
+//       },
+//     },
+//   };
+//   return (
+//     <React.Fragment>
+//       <Typography align="center">Irms</Typography>
+//       <ReactApexChart
+//         options={options}
+//         series={series}
+//         type="area"
+//         height={350}
+//       />
+//     </React.Fragment>
+//   );
+// }
+// export default HistoryAderelayChartIrms;
--- src/components/Chart/Aderelaychartcomponent/historyAde/HistoryAderelayChart.Power.js
+++ src/components/Chart/Aderelaychartcomponent/historyAde/HistoryAderelayChart.Power.js
@@ -1,59 +1,59 @@
-import React from "react";
-import ReactApexChart from "react-apexcharts";
+// import React from "react";
+// import ReactApexChart from "react-apexcharts";
 
-import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
-import { getRecords } from "../../../../api";
-import { Typography } from "@mui/material";
+// import { useParams } from "react-router-dom";
+// import { useQuery } from "react-query";
+// import { getRecords } from "../../../../api";
+// import { Typography } from "@mui/material";
 
-function HistoryAderelayChartPower() {
-  const { deviceId } = useParams();
-  const { data: power } = useQuery(
-    "Records1",
-    () => getRecords(deviceId, "power1"),
-    {
-      initialData: [],
-    }
-  );
+// function HistoryAderelayChartPower() {
+//   const { deviceId } = useParams();
+//   const { data: power } = useQuery(
+//     "Records1",
+//     () => getRecords(deviceId, "power"),
+//     {
+//       initialData: [],
+//     }
+//   );
 
-  const series = [
-    {
-      data: power.map(({ timestamp, value }) => ({
-        x: timestamp,
-        y: value,
-      })),
-    },
-  ];
-  const options = {
-    chart: {
-      height: 350,
-      type: "area",
-      dataLabels: {
-        enabled: false,
-      },
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "datetime",
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy HH:mm",
-      },
-    },
-  };
-  return (
-    <React.Fragment>
-      <Typography align="center">Power</Typography>
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="area"
-        height={350}
-      />
-    </React.Fragment>
-  );
-}
-export default HistoryAderelayChartPower;
+//   const series = [
+//     {
+//       data: power.map(({ timestamp, value }) => ({
+//         x: timestamp,
+//         y: value,
+//       })),
+//     },
+//   ];
+//   const options = {
+//     chart: {
+//       height: 350,
+//       type: "area",
+//       dataLabels: {
+//         enabled: false,
+//       },
+//     },
+//     stroke: {
+//       curve: "smooth",
+//     },
+//     xaxis: {
+//       type: "datetime",
+//     },
+//     tooltip: {
+//       x: {
+//         format: "dd/MM/yy HH:mm",
+//       },
+//     },
+//   };
+//   return (
+//     <React.Fragment>
+//       <Typography align="center">Power</Typography>
+//       <ReactApexChart
+//         options={options}
+//         series={series}
+//         type="area"
+//         height={350}
+//       />
+//     </React.Fragment>
+//   );
+// }
+// export default HistoryAderelayChartPower;
