20635bd3903017ead4ff3b0cf76de764de55ebc2
phamducai <99159542+phamducai@users.noreply.github.com>
2022-05-30 17:49:27 +0700

    hahah

--- src/components/Chart/Adehistory.js
+++ src/components/Chart/Adehistory.js
@@ -1,86 +1,29 @@
 import React from "react";
-import ReactApexChart from "react-apexcharts";
 
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
-import { getRecords } from "../../api";
+import { getDeviceById, getRoomById } from "../../api";
+import { Typography, Divider } from "@mui/material";
+import AdeVrms from "./Adehistorycomponent/AdeVrms";
+import AdeIrms from "./Adehistorycomponent/AdeIrms";
+import AdePower from "./Adehistorycomponent/AdePower";
 
 function Adehistory() {
   const { deviceId } = useParams();
-  const { data: vrms } = useQuery(
-    "Records1",
-    () => getRecords(deviceId, "vrms"),
-    {
-      initialData: [],
-    }
+  const { data: device } = useQuery(["devicessssss", deviceId], () =>
+    getDeviceById(deviceId)
   );
-  const { data: irms } = useQuery(
-    "Records2",
-    () => getRecords(deviceId, "irms"),
-    {
-      initialData: [],
-    }
-  );
-  const { data: power } = useQuery(
-    "Records3",
-    () => getRecords(deviceId, "power"),
-    {
-      initialData: [],
-    }
-  );
-
-  const series = [
-    {
-      name: "Vrms",
-      data: vrms.map(({ timestamp, value }) => ({
-        x: timestamp,
-        y: value,
-      })),
-    },
-    {
-      name: "irms",
-      data: irms.map(({ timestamp, value }) => ({
-        x: timestamp,
-        y: value,
-      })),
-    },
-    {
-      name: "power",
-      data: power.map(({ timestamp, value }) => ({
-        x: timestamp,
-        y: value,
-      })),
-    },
-  ];
-  const options = {
-    chart: {
-      height: 350,
-      type: "area",
-      dataLabels: {
-        enabled: false,
-      },
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "datetime",
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy HH:mm",
-      },
-    },
-  };
 
   return (
     <React.Fragment>
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="area"
-        height={350}
-      />
+      <Typography variant="h4" align="center">
+        {device?.name}
+      </Typography>
+      <AdeVrms />
+      <Divider />
+      <AdeIrms />
+      <Divider />
+      <AdePower />
     </React.Fragment>
   );
 }
